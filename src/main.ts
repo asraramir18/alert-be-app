6b0b7586a2be9379d0737cadbf638a544de2e5f7
Muhammad Asrar Amir <Muhammad.Asrar@fwd.com>
2022-07-23 15:40:22 +0800

    Cors issue

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const allowList = ['https://riza-asrar-wedding.herokuapp.com/']
-  const corsOptionsDelegate = function (req, callback) {
-    let corsOptions;
-    if (allowList.indexOf(req.header('Origin')) !== -1) {
-      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-    } else {
-      corsOptions = { origin: false } // disable CORS for this request
-    }
-    callback(null, corsOptions) // callback expects two parameters: error and options
-  }
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
